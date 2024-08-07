5dccbce9249563309a26d58a342ceadd8ba7007b
Adithya <kasarlaadithya642@gmail.com>
2024-08-07 18:44:15 -0500

    working with components

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const parent=React.createElement("div",
-{id:"div1"},[
-React.createElement("div",
-{id:"div2"},
-[React.createElement("h1",
-{id:"h1"},
-"This is a h1 tag"),
-React.createElement("h2",
-{id:"h2"},
-"This is a h2 tag")]),React.createElement("div",
-{id:"div2"},
-[React.createElement("h1",
-{id:"h1"},
-"This is a h1 tag"),
-React.createElement("h2",
-{id:"h2"},
-"This is a h2 tag")])]);
-console.log(parent);
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//  const head=React.createElement("h1",
+//  {id:"head"},
+//  "reatc is rendered here");
+//jsx codes are formatted so that browswers could understand -by parsel
+ const Heading=()=>{
+    return (<h1 className="head" tabIndex="5">
+    react is rendered1
+    </h1>)};
+    //componenet composition
+const HeadComponent=()=>(
+        <div id="container">
+            {Heading()}
+            <Heading/>
+            <Heading></Heading>
+         <h1> Hello namasthe</h1>
+        </div>
+);
+  
+ const initial=ReactDOM.createRoot(document.getElementById("root"));
+ initial.render(<HeadComponent/>);
